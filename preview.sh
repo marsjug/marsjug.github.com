2898b7aaf62f507b1127b83a7e0ce7aa2af405ef
Aurelie Vache <scraly@gmail.com>
2025-02-13 19:41:28 +0000

    feat: add a script that serve the marsjug website for preview/debugging purpose

--- preview.sh
+++ preview.sh
@@ -3,14 +3,13 @@
 # if java version != 21
 
 #prerequisites - install sdkman, java and quarkus
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
-sdk install java 21.0.5-tem
-sdk install quarkus 
-
-curl -Ls https://sh.jbang.dev | bash -s - app setup
-jbang app install --verbose --fresh --force roq@quarkiverse/quarkus-roq
+# curl -s "https://get.sdkman.io" | bash
+# source "$HOME/.sdkman/bin/sdkman-init.sh"
+# sdk install java 21.0.5-tem
+# sdk install quarkus
+# sdk install jbang
+# jbang trust add https://github.com/quarkiverse/quarkus-roq/blob/HEAD/roq.java
+# jbang app install --verbose --fresh --force roq@quarkiverse/quarkus-roq
 
 # generate static website to target/roq
 quarkus build
